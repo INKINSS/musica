efca2b616913be70404a603d54cae245f9fec11a
samito <martinsamuelpp@gmail.com>
2024-10-04 14:56:32 -0600

    Actualizar dependencia de Firebase en firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,14 +10,14 @@ const firebaseConfig = {
   apiKey: "AIzaSyB83I-9r0MaxFr_zMOJ-jLLnXs1-Zj118A",
   authDomain: "appmusic-a466c.firebaseapp.com",
   projectId: "appmusic-a466c",
-  storageBucket: "appmusic-a466c.appspot.com",
+  storageBucket: "gs://appmusic-a466c.appspot.com",
   messagingSenderId: "123750166497",
   appId: "1:123750166497:web:9ca59789306a7a8384aca0",
-  measurementId: "G-W0HKMCMT4G"
+  measurementId: "G-W0HKMCMT4G",  
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const storage = getAnalytics(app);
+const storage = getStorage(app);
 
 export default storage;
